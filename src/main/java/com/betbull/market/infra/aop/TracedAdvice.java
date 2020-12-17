473193f0d6c7b697131898292cd9440153b5d96f
Vlad Martyshevskyi <vladmartishevskii@gmail.com>
2020-12-17 14:47:20 +0200

    Added around advice

--- src/main/java/com/betbull/market/infra/aop/TracedAdvice.java
+++ src/main/java/com/betbull/market/infra/aop/TracedAdvice.java
@@ -1,6 +1,7 @@
 package com.betbull.market.infra.aop;
 
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,9 +13,13 @@ public class TracedAdvice {
 
     private final static Logger LOG = LoggerFactory.getLogger(TracedAdvice.class);
 
-    @After("execution(* com.betbull.market.service.impl.mail.GoogleMailServiceImpl.*(..)) && @annotation(Traced) ")
-    public void trace() {
-        LOG.info("Tracing aspect log");
+    @Around("execution(* com.betbull.market.service.impl.mail.GoogleMailServiceImpl.*(..)) && @annotation(Traced)")
+    public Object trace(ProceedingJoinPoint joinPoint) throws Throwable{
+        long startTime = System.currentTimeMillis();
+        Object result = joinPoint.proceed(joinPoint.getArgs());
+        LOG.debug("Tracing aspect: invocation of " + joinPoint.getSignature().getName() + " took " + (System.currentTimeMillis() - startTime));
+        return result;
     }
+
 
 }
